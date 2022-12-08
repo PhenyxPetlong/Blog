b44e7c471a5ba47827c730f83c60b45994650f7e
PhenyxPetlong <prowsypetlong@gmail.com>
2022-12-08 03:33:46 +0100

    first commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{ 
+   public function register(Request $request)
+   {
+    
+
+   }
+
+   public function login()
+   {
+    
+   }
+    //
+}
